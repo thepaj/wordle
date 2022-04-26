0debb35237ec9b3ac1b98e094edacd72eaa6d037
Paja Jureckova <pavla.jureckova@gmail.com>
2022-04-26 18:29:49 +0200

    example of a single row working

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,7 @@ import Keyboard from "./Keyboard";
 import { useState } from "react";
 
 function Board(props) {
-    const [word, setWord] = useState('');
+    const [word, setWord] = useState(['']);
 
     // import props
     const wordToGuess = props.wordToGuess;
@@ -11,19 +11,20 @@ function Board(props) {
 
     // gets a word from the keyboard child component
     const getWord = (wrd) => {
-        console.log(`word: ${wrd}`);
-        setWord(wrd)
+        setWord((prevState) => {
+            return [wrd]
+        })
     }
 
     return(
         <div className='board-container'>
             <div className='word-row-container'>
+                <WordRow word={word[0]} />
+                {/* <WordRow word={word} />
                 <WordRow word={word} />
                 <WordRow word={word} />
                 <WordRow word={word} />
-                <WordRow word={word} />
-                <WordRow word={word} />
-                <WordRow word={word} />
+                <WordRow word={word} /> */}
             </div>
             <div className='keyboard-container'>
                 <Keyboard callback={getWord} gameStarted={gameStarted} wordToGuess={wordToGuess}/>
